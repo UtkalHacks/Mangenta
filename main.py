f19ac0d549b1dff3187db22424ca0563bdc2ac32
raffleberry <raffleberry42@gmail.com>
2019-03-17 16:46:01 +0530

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from api import object_counting_api
 if tf.__version__ < '1.4.0':
   raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
 
-input_video = 'ok.jpeg'
+input_video = 0
 
 # We use "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 detection_graph, category_index = backbone.set_model('ssdlite_mobilenet_v2_coco_2018_05_09')
